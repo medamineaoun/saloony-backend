d9c40a3706075423b729530d642849389b05348f
maaoun <maaoun@vermeg.com>
2025-05-14 12:55:35 +0100

    Ajout des services : salon, disponibilité et réservation

---
+++ Availability-management/src/test/java/com/aoun/availabilitymanagement/AvailabilityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoun.availabilitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailabilityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
