eda78d7ec630cbd17bc68315383e141dacfa09d4
maaoun <maaoun@vermeg.com>
2025-05-14 21:05:54 +0100

    ajout projet serveur Eureka

---
+++ Eureka/src/test/java/com/aoun/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoun.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
