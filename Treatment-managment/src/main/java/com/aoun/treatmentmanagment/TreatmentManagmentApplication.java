28608af3aa2acaf1ac9b6a05e22a0cfa31e6e931
maaoun <maaoun@vermeg.com>
2025-07-13 17:42:23 +0100

    configuration de eureka-server avec toutes les microservices(sauf module notification avec node js)

--- Treatment-managment/src/main/java/com/aoun/treatmentmanagment/TreatmentManagmentApplication.java
+++ Treatment-managment/src/main/java/com/aoun/treatmentmanagment/TreatmentManagmentApplication.java
@@ -2,8 +2,10 @@ package com.aoun.treatmentmanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TreatmentManagmentApplication {
 
 	public static void main(String[] args) {
